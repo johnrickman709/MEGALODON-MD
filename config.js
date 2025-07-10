953a4b216dc024bc6c08dc2e822f1dfe7896d164
johnrickman709 <osarumwenseosadolor85@gmail.com>
2025-07-10 05:31:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on",
     //chatbot on/off
   
-    SESSION_ID: process.env.SESSION_ID || "",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~oVIDxbJK#dhhJi6oX3eaUMKFDxMod07A0QQOLFOFkOahAmRnWV0E",  // Your bot's session ID (keep it secure)
     
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     
